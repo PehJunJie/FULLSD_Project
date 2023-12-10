a043ab559319189ba5ccee51468b6e272fd0d43c
jjpeh <jjpeh@LAPTOP-I971RUIG>
2023-12-10 21:47:31 +0800

    Done identity server. Done register and login in nav menu.

--- GameVerse/Server/Models/ApplicationUser.cs
+++ GameVerse/Server/Models/ApplicationUser.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using GameVerse.Client;
+using Humanizer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Text;
+using System.Threading.Channels;
 
 namespace GameVerse.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
